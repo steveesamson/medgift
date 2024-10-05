fb9c3d65df8412f7f8f7efb9636b2c41a9b5ce99
Steve S. Samson <stevee.samson@gmail.com>
2024-10-05 14:40:28 -0400

    Signup Page: add viewBinding and the Register Activity

--- app/src/main/java/com/apollo/medgift/RegisterActivity.java
+++ app/src/main/java/com/apollo/medgift/RegisterActivity.java
@@ -1,48 +1,50 @@
 package com.apollo.medgift;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.content.Intent;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.apollo.medgift.databinding.ActivityMainBinding;
+import com.apollo.medgift.databinding.ActivityRegisterBinding;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    private ActivityMainBinding mainBinding;
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+    private ActivityRegisterBinding registerBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        this.registerBinding = ActivityRegisterBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(mainBinding.getRoot());
-        ViewCompat.setOnApplyWindowInsetsListener(mainBinding.main, (v, insets) -> {
+
+        setContentView(registerBinding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(registerBinding.registerForm, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
         bindEvents();
     }
 
     private void bindEvents() {
-        this.mainBinding.btnSignup.setOnClickListener(this);
+        this.registerBinding.btnLogin.setOnClickListener(this);
     }
 
 
-    private void register(){
-        Intent intent = new Intent(this, RegisterActivity.class);
+    private void login(){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
     @Override
     public void onClick(View view) {
         int vId = view.getId();
-        if(vId == this.mainBinding.btnSignup.getId()){
-            register();
+        if(vId == this.registerBinding.btnLogin.getId()){
+            login();
         }
     }
 }
