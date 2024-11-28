91ec8dc5b17f31f4c00296948603a01a218628c2
Steve S. Samson <stevee.samson@gmail.com>
2024-11-28 18:42:25 -0500

    fix page errors, add binding and set page title

--- app/src/main/java/com/apollo/medgift/views/gifter/PaymentInformationActivity.java
+++ app/src/main/java/com/apollo/medgift/views/gifter/PaymentInformationActivity.java
@@ -1,6 +1,8 @@
 package com.apollo.medgift.views.gifter;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +11,38 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.apollo.medgift.R;
+import com.apollo.medgift.common.BaseActivity;
+import com.apollo.medgift.common.Util;
+import com.apollo.medgift.databinding.ActivityPaymentinformationBinding;
+import com.apollo.medgift.models.HealthcareService;
+import com.apollo.medgift.models.Recipient;
 
-public class PaymentInformationActivity extends AppCompatActivity {
+public class PaymentInformationActivity extends BaseActivity {
+
+    private ActivityPaymentinformationBinding binding;
+    private HealthcareService service;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityPaymentinformationBinding.inflate(getLayoutInflater());
+
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_paymentinformation);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(binding.getRoot());
+        applyWindowInsetsListenerTo(this, binding.main);
+        Intent intent = getIntent();
+        service = (HealthcareService) intent.getSerializableExtra(HealthcareService.STORE);
+
+        setupToolbar(binding.homeAppBar.getRoot(), "Service Details", true);
+        setupActivity();
+    }
+
+    private void setupActivity() {
+        binding.btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
         });
     }
 }
