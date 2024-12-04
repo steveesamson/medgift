d1e1c2db9cb9d6c440e81380de1dc13194fad7df
Steve S. Samson <stevee.samson@gmail.com>
2024-12-04 09:14:06 -0500

    add noti details for invite and services

--- app/src/main/java/com/apollo/medgift/views/AlertDetail.java
+++ app/src/main/java/com/apollo/medgift/views/AlertDetail.java
@@ -4,73 +4,50 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.apollo.medgift.common.BaseActivity;
-import com.apollo.medgift.common.Closeable;
-import com.apollo.medgift.common.Firebase;
 import com.apollo.medgift.databinding.ActivityAlertdetailBinding;
-import com.apollo.medgift.models.Gift;
 import com.apollo.medgift.models.GiftInvite;
 import com.apollo.medgift.models.GiftService;
-import com.apollo.medgift.models.HealthcareService;
 import com.apollo.medgift.models.Message;
-import com.apollo.medgift.views.gifter.ServiceDetailActivity;
+import com.apollo.medgift.models.NotificationType;
 
 public class AlertDetail extends BaseActivity {
 
     private ActivityAlertdetailBinding binding;
     private Message message;
-    private Closeable closeable;
-    private Gift gift;
-    private HealthcareService service;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityAlertdetailBinding.inflate(getLayoutInflater());
-
+        binding = ActivityAlertdetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        applyWindowInsetsListenerTo(this, binding.main);
         Intent intent = getIntent();
         message = (Message)intent.getSerializableExtra(Message.STORE);
         assert message != null;
-        switch (message.getNotificationType()){
-            case  GiftService:
-                GiftService gs = (GiftService)message.getPayLoad();
-                closeable = Firebase.getModelBy(HealthcareService.STORE,"key", gs.getServiceId(),HealthcareService.class,(svc) ->{
-                    closeable.release();
-                    service = svc;
-                    setupActivity();
-                });
-                break;
-            case GiftInvite:
-                GiftInvite gi = (GiftInvite)message.getPayLoad();
-                closeable = Firebase.getModelBy(Gift.STORE,"key", gi.getGiftId(),Gift.class,(gft) ->{
-                    closeable.release();
-                    gift = gft;
-                    setupActivity();
-                });
-                break;
-        }
 
-
+       setupActivity();
 
     }
 
     private void setupActivity() {
-        binding = ActivityAlertdetailBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        applyWindowInsetsListenerTo(this, binding.main);
+
         binding.alertTitle.setText(message.getTitle());
         binding.alertContent.setText(message.getBody());
-        binding.alterButton.setText(message.getButtonLabel());
+//        binding.alterButton.setText(message.getButtonLabel());
 
         binding.alterButton.setOnClickListener((v) ->{
             Intent outIntent;
             switch (message.getNotificationType()){
-                case  GiftService:
-                    outIntent = new Intent(this, ServiceDetailActivity.class);
-                    outIntent.putExtra(HealthcareService.STORE, service);
+                case GiftService:
+                    outIntent = new Intent(this, ServiceInfoActivity.class);
+                    GiftService giftService = (GiftService)message.getPayLoad();
+                    outIntent.putExtra(GiftService.STORE, giftService);
                     startActivity(outIntent);
                     break;
                 case GiftInvite:
-                     outIntent = new Intent(this,HealthcareService.class);
-                        outIntent.putExtra(Gift.STORE, gift);
+                     outIntent = new Intent(this,InviteInfoActivity.class);
+                    GiftInvite giftInvite = (GiftInvite) message.getPayLoad();
+                        outIntent.putExtra(GiftInvite.STORE, giftInvite);
                         startActivity(outIntent);
                     break;
             }
